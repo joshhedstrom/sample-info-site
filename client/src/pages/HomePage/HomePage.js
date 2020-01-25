ae8a5af3ddc9b29d78551f6196283f08f80e998e
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-01-25 15:43:42 -0700

    scaffold home page

---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+class HomePage extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="homepage__wrapper">
+      </div>
+    );
+  }
+}
+
+export default HomePage;
