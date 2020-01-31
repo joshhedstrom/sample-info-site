7a0fbdd2a5ca9ec3066b0b49810f783f33f39934
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-01-30 20:56:28 -0700

    imported components

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
+import Navbar from '../../components/Navbar';
 import './index.css';
+import Jumbotron from '../../components/Jumbotron/Jumbotron';
+import Paragraph from '../../components/Paragraph/Paragraph';
 
 class HomePage extends Component {
   state = {};
@@ -8,6 +11,16 @@ class HomePage extends Component {
   render() {
     return (
       <div className="homepage__wrapper">
+        <div className="landing__page__wrapper">
+          <Navbar />
+          <Jumbotron text="country church" />
+        </div>
+        <div>
+        </div>
+        {/* <div className="mission_statement__wrapper"> */}
+          <Paragraph />
+        {/* </div> */}
+            
       </div>
     );
   }
