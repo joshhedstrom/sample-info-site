2614d5e086ca4f487284539355df1e95f6b3dcdc
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-03 20:44:04 -0700

    abstracted frosted glass effect to page

--- client/src/components/Paragraph/Paragraph.js
+++ client/src/components/Paragraph/Paragraph.js
@@ -4,27 +4,25 @@ import './index.css';
 
 function Paragraph(props) {
   return (
-    <div className="paragraph__wrapper">
-      <p className="paragraph__text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Parturient montes
-        nascetur ridiculus mus mauris vitae. Ullamcorper a lacus vestibulum sed
-        arcu. Sit amet risus nullam eget. Aliquet nec ullamcorper sit amet risus
-        nullam eget felis. Vel facilisis volutpat est velit. Tortor condimentum
-        lacinia quis vel eros donec ac odio tempor. Egestas egestas fringilla
-        phasellus faucibus. Nisl pretium fusce id velit ut. A diam sollicitudin
-        tempor id eu nisl. Velit ut tortor pretium viverra suspendisse potenti
-        nullam ac. Risus feugiat in ante metus dictum. Amet tellus cras
-        adipiscing enim eu turpis. Ipsum dolor sit amet consectetur. Malesuada
-        fames ac turpis egestas. Maecenas volutpat blandit aliquam etiam. Quam
-        nulla porttitor massa id neque aliquam. A cras semper auctor neque vitae
-        tempus quam pellentesque nec. Lobortis feugiat vivamus at augue eget
-        arcu dictum varius duis. Pharetra et ultrices neque ornare aenean
-        euismod elementum nisi quis. In eu mi bibendum neque egestas congue
-        quisque. Pretium nibh ipsum consequat nisl vel pretium lectus quam. Sed
-        adipiscing diam donec adipiscing.
-      </p>
-    </div>
+    <p className="paragraph__text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Parturient montes
+      nascetur ridiculus mus mauris vitae. Ullamcorper a lacus vestibulum sed
+      arcu. Sit amet risus nullam eget. Aliquet nec ullamcorper sit amet risus
+      nullam eget felis. Vel facilisis volutpat est velit. Tortor condimentum
+      lacinia quis vel eros donec ac odio tempor. Egestas egestas fringilla
+      phasellus faucibus. Nisl pretium fusce id velit ut. A diam sollicitudin
+      tempor id eu nisl. Velit ut tortor pretium viverra suspendisse potenti
+      nullam ac. Risus feugiat in ante metus dictum. Amet tellus cras adipiscing
+      enim eu turpis. Ipsum dolor sit amet consectetur. Malesuada fames ac
+      turpis egestas. Maecenas volutpat blandit aliquam etiam. Quam nulla
+      porttitor massa id neque aliquam. A cras semper auctor neque vitae tempus
+      quam pellentesque nec. Lobortis feugiat vivamus at augue eget arcu dictum
+      varius duis. Pharetra et ultrices neque ornare aenean euismod elementum
+      nisi quis. In eu mi bibendum neque egestas congue quisque. Pretium nibh
+      ipsum consequat nisl vel pretium lectus quam. Sed adipiscing diam donec
+      adipiscing.
+    </p>
   );
 }
 
