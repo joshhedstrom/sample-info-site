05f7a30936229126e6f61a679a0106e792ee3ebc
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-03 20:45:07 -0700

    scaffolded contact page

---
+++ client/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Navbar from '../../components/Navbar';
+import './index.css';
+import Jumbotron from '../../components/Jumbotron/Jumbotron';
+import Paragraph from '../../components/Paragraph/Paragraph';
+
+class ContactPage extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="contactpage__wrapper">
+      </div>
+    );
+  }
+}
+
+export default ContactPage;
