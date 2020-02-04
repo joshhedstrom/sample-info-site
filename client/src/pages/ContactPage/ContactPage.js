ebcd1b7f42a45227b86baf4283cb5f2810acb63a
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-03 21:39:58 -0700

    scaffolded form fields

--- client/src/pages/ContactPage/ContactPage.js
+++ client/src/pages/ContactPage/ContactPage.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 
 import Navbar from '../../components/Navbar';
-import './index.css';
-import Jumbotron from '../../components/Jumbotron/Jumbotron';
-import Paragraph from '../../components/Paragraph/Paragraph';
+import Form from '../../components/Form/Form';
 
+import './index.css';
 class ContactPage extends Component {
-  state = {};
+  state = {
+    formFields: [
+      {
+        textarea: false,
+        title: 'Name',
+        required: true
+      },
+      {
+        textarea: false,
+        title: 'Email Address',
+        required: true
+      },
+      {
+        textarea: false,
+        title: 'Phone Number',
+        required: false
+      },
+      {
+        textarea: true,
+        title: 'Comment',
+        required: true
+      }
+    ]
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    //validate fields
+    //make api call with this.state
+  };
 
   render() {
     return (
       <div className="contactpage__wrapper">
+        <Navbar />
+        <Form
+          {...this.state}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
