28711d5699f6dbb273a3258af759bceabf84f988
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-06 21:07:30 -0700

    completed banner component

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -3,9 +3,7 @@ import React from 'react';
 import './index.css';
 
 function Banner(props) {
-	return (
-		<div></div>
-	)
+  return <div className="banner__image" style={{ backgroundImage: `url(${props.image})` }} />;
 }
 
 export default Banner;
