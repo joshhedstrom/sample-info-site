3962a4ab22836e5d6289bdb7827d7bf7ca59a8b6
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-08 20:52:26 -0700

    added contact form at bottom of page

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -4,22 +4,68 @@ import Navbar from '../../components/Navbar';
 import './index.css';
 import Jumbotron from '../../components/Jumbotron/Jumbotron';
 import Paragraph from '../../components/Paragraph/Paragraph';
+import Form from '../../components/Form';
 
 class HomePage extends Component {
-  state = {};
+  state = {
+    formFields: [
+      {
+        textArea: false,
+        title: 'Name',
+        name: 'name'
+      },
+      {
+        textArea: false,
+        title: 'Email Address',
+        name: 'email'
+      },
+      {
+        textArea: false,
+        title: 'Phone Number',
+        name: 'number'
+      },
+      {
+        textArea: true,
+        title: 'Comment',
+        name: 'comment'
+      }
+    ]
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    let formObject = {
+      name: this.state.name,
+      email: this.state.email,
+      number: this.state.number,
+      comment: this.state.comment
+    };
+
+    console.log(formObject);
+    //validate fields
+    //make api call with this.state
+  };
 
   render() {
     return (
       <div className='homepage__wrapper'>
-        <div className='landing__page__wrapper scroll__child'>
+        <div className='logo__wrapper scroll__child'>
           <Navbar />
           <div className='homepage__jumbotron'>
             <Jumbotron text='classic clothing' />
           </div>
         </div>
-        <div className='mission_statement_wrapper scroll__child'>
-          <div className='paragraph__wrapper'>
+        <div className='center__wrapper scroll__child'>
+          <div className='frosted__glass__wrapper'>
             <Paragraph />
+          </div>
+        </div>
+        <div className='center__wrapper scroll__child'>
+          <div className='frosted__glass__wrapper'>
+            <Form {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
           </div>
         </div>
       </div>
