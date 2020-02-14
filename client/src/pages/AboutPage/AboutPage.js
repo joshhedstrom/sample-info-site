9a7efb5236d527ecfa9f0f311ff20e3b129a4a1b
Josh Hedstrom <josh.hedstrom@gmail.com>
2020-02-13 21:28:32 -0700

    scaffolded logic for transitions

--- client/src/pages/AboutPage/AboutPage.js
+++ client/src/pages/AboutPage/AboutPage.js
@@ -4,23 +4,95 @@ import Navbar from '../../components/Navbar';
 
 import './index.css';
 class AboutPage extends Component {
-  state = {};
+  
+    componentDidMount() {
+      window.addEventListener('scroll', this.incrementActiveProfile);
+    }
+  
+    componentWillUnmount() {
+      window.removeEventListener('scroll', this.incrementActiveProfile);
+    }
+
+
+  state = {
+    activeProfile: 1,
+    teamMembers: [
+      {
+        name: 'Sally',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Sam',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Steve',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Susie',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Savannah',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Sebastian',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Silas',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      },
+      {
+        name: 'Stella',
+        bio:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere atque culpa repellendus voluptatum nemo, deserunt praesentium quisquam laborum, aut excepturi odio nam? Hic eveniet nisi nulla culpa nihil voluptate quisquam.'
+      }
+    ]
+  };
+
+  handleScroll = event => {
+    if(event.deltaY < 0) {
+      this.decrementActiveProfile(event)
+    } else if(event.deltaY > 0) {
+      this.incrementActiveProfile(event)
+    }
+  }
+
+  incrementActiveProfile = event => {
+    if (this.state.activeProfile < 8) {
+      this.setState({ activeProfile: this.state.activeProfile + 1 });
+    } else if (this.state.activeProfile >= 8) {
+      this.setState({ activeProfile: 8 });
+    }
+  };
+
+  decrementActiveProfile = event => {
+    if (this.state.activeProfile <= 8) {
+      this.setState({ activeProfile: this.state.activeProfile - 1 });
+    } else if (this.state.activeProfile >= 1) {
+      this.setState({ activeProfile: 1 });
+    }
+  };
 
   render() {
     return (
-      <div className='aboutpage__wrapper'>
+      <div className='aboutpage__wrapper' onWheel={this.handleScroll}>
         <Navbar />
         <div className='team__image'>
-          <div className='profile__circle'></div>
-          <div id='user1' />
-          <div id='user2' />
-          <div id='user3' />
-          <div id='user4' />
-          <div id='user5' />
-          <div id='user6' />
-          <div id='user7' />
-          <div id='user8' />
+          <div className='profile__circle' id={`user${this.state.activeProfile}`}></div>
         </div>
+        <h2>{this.state.teamMembers[this.state.activeProfile - 1].name}</h2>
+        <p>{this.state.teamMembers[this.state.activeProfile - 1].bio}</p>
       </div>
     );
   }
